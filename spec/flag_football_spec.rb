22d8d74eac7c2d39d360b3955384bb09e218da52
Max Pleaner <max.pleaner@hover.to>
2020-04-17 11:51:43 -0700

    auto-generated specs pass

--- spec/flag_football_spec.rb
+++ spec/flag_football_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe FlagFootball do
     expect(FlagFootball::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
