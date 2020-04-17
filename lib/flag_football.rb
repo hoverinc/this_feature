dba3c039caa2e40b48cbf2587dc07a420a6da9ba
Max Pleaner <max.pleaner@hover.to>
2020-04-17 11:53:22 -0700

    remove some auto-generateed stuff

--- lib/flag_football.rb
+++ lib/flag_football.rb
@@ -2,5 +2,4 @@ require "flag_football/version"
 
 module FlagFootball
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/flag_football_spec.rb
+++ spec/flag_football_spec.rb
@@ -1,9 +1,7 @@
 RSpec.describe FlagFootball do
+
   it "has a version number" do
     expect(FlagFootball::VERSION).not_to be nil
   end
 
-  # it "does something useful" do
-  #   expect(false).to eq(true)
-  # end
 end
