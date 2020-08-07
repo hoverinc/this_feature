50a80502409fb37ff02d9ddf3fe6d397e6f80547
Matt Fong <matthewjf@gmail.com>
2020-08-07 13:09:39 -0700

    adds default adapter support and spec for FFeature.adapter_for (#24)

--- lib/ffeature/adapters/base.rb
+++ lib/ffeature/adapters/base.rb
@@ -6,15 +6,15 @@ class FFeature
         raise UnimplementedError.new(self, __method__)
       end
 
+      def self.present?(flag_name, context: nil, data: {})
+        raise UnimplementedError.new(self, __method__)
+      end
+
       def self.on?(flag_name, context: nil, data: {})
         raise UnimplementedError.new(self, __method__)
       end
 
       def self.off?(flag_name, context: nil, data: {})
-        raise UnimplementedError.new(self, __method__)
-      end
-
-      def self.control?(flag_name, context: nil, data: {})
         raise UnimplementedError.new(self, __method__)
       end
 
@@ -26,6 +26,11 @@ class FFeature
         raise UnimplementedError.new(self, __method__)
       end
 
+      # OPTIONAL method
+      # check to see if a control is being used
+      def self.control?(flag_name, context: nil, data: {})
+        false
+      end
     end
   end
 end
--- spec/ffeature/adapters/base_adapter_spec.rb
+++ spec/ffeature/adapters/base_adapter_spec.rb
@@ -3,7 +3,7 @@ klass = FFeature::Adapters::Base
 RSpec.shared_examples "examples for interface method" do |fn_name, *args|
   it "raises an unimplemented error for `#{fn_name}` with args #{args}`" do
     expect do
-      klass.public_send(fn_name, *args)
+      described_class.public_send(fn_name, *args)
     end.to raise_error(FFeature::UnimplementedError) do |err|
       expect(err.message).to eq("class #{klass.name} doesnt implement method .#{fn_name}")
     end
@@ -13,15 +13,18 @@ end
 RSpec.describe klass do
   context "methods providing an interface for descendent classes to implement" do
     include_examples "examples for interface method", :setup
+    include_examples "examples for interface method", :present?, :some_flag_name
     include_examples "examples for interface method", :on?, :some_flag_name
     include_examples "examples for interface method", :on?, :some_flag_name, context: :fake_user, data: :stuff
     include_examples "examples for interface method", :off?, :some_flag_name
     include_examples "examples for interface method", :off?, :some_flag_name, context: :fake_user, data: :stuff
-    include_examples "examples for interface method", :control?, :some_flag_name
-    include_examples "examples for interface method", :control?, :some_flag_name, context: :fake_user, data: :stuff
     include_examples "examples for interface method", :on!, :some_flag_name
     include_examples "examples for interface method", :on!, :some_flag_name, context: :fake_user, data: :stuff
     include_examples "examples for interface method", :off!, :some_flag_name
     include_examples "examples for interface method", :off!, :some_flag_name, context: :fake_user, data: :stuff
+
+    it 'implements #control? and returns false by default' do
+      expect(described_class.control?(:some_flag_name)).to eq(false)
+    end
   end
 end
