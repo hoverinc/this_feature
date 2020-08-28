da71ccad7eb39d1b4bb654d9022236e7336f5c8b
matthew fong <matthew@hover.to>
2020-08-28 14:53:22 -0700

    migrate memory adapter to instance api

--- lib/this_feature/adapters/memory.rb
+++ lib/this_feature/adapters/memory.rb
@@ -2,19 +2,19 @@ class ThisFeature
   module Adapters
     class Memory < Base
 
-      def self.setup(context_id_method: :id)
+      def initialize(context_id_method: :id)
         @context_id_method = context_id_method
       end
 
-      def self.clear
+      def clear
         storage.clear
       end
 
-      def self.present?(flag_name)
+      def present?(flag_name)
         !storage[flag_name].nil?
       end
 
-      def self.on?(flag_name, context: nil, data: {})
+      def on?(flag_name, context: nil, data: {})
         # binding.pry
         return unless present?(flag_name)
 
@@ -28,7 +28,7 @@ class ThisFeature
         !!flag_data[:contexts][context.send(@context_id_method)]
       end
 
-      def self.off?(flag_name, context: nil, data: {})
+      def off?(flag_name, context: nil, data: {})
         on_result = on?(flag_name, context: context)
 
         return if on_result.nil?
@@ -36,7 +36,7 @@ class ThisFeature
         !on_result
       end
 
-      def self.on!(flag_name, context: nil, data: {})
+      def on!(flag_name, context: nil, data: {})
         storage[flag_name] ||= {}
 
         return storage[flag_name][:global] = true if context.nil?
@@ -45,7 +45,7 @@ class ThisFeature
         storage[flag_name][:contexts][context.send(@context_id_method)] = true
       end
 
-      def self.off!(flag_name, context: nil, data: {})
+      def off!(flag_name, context: nil, data: {})
         storage[flag_name] ||= {}
 
         return storage[flag_name][:global] = false if context.nil?
@@ -54,7 +54,7 @@ class ThisFeature
         storage[flag_name][:contexts][context.send(@context_id_method)] = false
       end
 
-      def self.storage
+      def storage
         @storage ||= {}
       end
     end
--- spec/this_feature/adapters/memory_adapter_spec.rb
+++ spec/this_feature/adapters/memory_adapter_spec.rb
@@ -7,47 +7,48 @@ RSpec.describe ThisFeature::Adapters::Memory do
   let(:pseudo_user) { OpenStruct.new(id: "User:1") }
   let(:pseudo_user2) { OpenStruct.new(id: "User:2") }
 
-  before(:each) do
-    described_class.clear
-  end
+  let(:adapter) { described_class.new }
+  let(:flag) { ThisFeature.flag(flag_name, context: context, data: data) }
 
-  describe ".setup" do
-    it "doesnt raise an error" do
-      described_class.setup
+  before(:each) do
+    adapter.clear
+
+    ThisFeature.configure do |config|
+      config.adapters = [adapter]
     end
   end
 
-  describe ".on?" do
-    subject(:on?) { described_class.on?(flag_name, context: context, data: data) }
+  describe "on?" do
+    subject(:on?) { flag.on? }
 
     context "looking up a flag that doesnt exist" do
       it { is_expected.to be_nil }
     end
 
     context "looking up a flag that is set to on" do
-      before { described_class.on!(flag_name) }
+      before { adapter.on!(flag_name) }
 
       it { is_expected.to be(true) }
     end
 
     context "looking up a flag that is set to off" do
-      before { described_class.off!(flag_name) }
+      before { adapter.off!(flag_name) }
 
       it { is_expected.to be(false) }
     end
 
     context "when given a flag name and a user" do
       it "qualifies the search" do
-        described_class.on!(flag_name, context: pseudo_user)
-        expect(described_class.on?(flag_name)).to be false
-        expect(described_class.on?(flag_name, context: pseudo_user2)).to be false
-        expect(described_class.on?(flag_name, context: pseudo_user)).to be true
+        adapter.on!(flag_name, context: pseudo_user)
+        expect(ThisFeature.flag(flag_name).on?).to be false
+        expect(ThisFeature.flag(flag_name, context: pseudo_user2).on?).to be false
+        expect(ThisFeature.flag(flag_name, context: pseudo_user).on?).to be true
       end
     end
   end
 
-  describe ".off?" do
-    subject(:off?) { described_class.off?(flag_name, context: context, data: data) }
+  describe "off?" do
+    subject(:off?) { flag.off? }
 
     context "looking up a flag that doesnt exist" do
       it "returns nil" do
@@ -56,85 +57,85 @@ RSpec.describe ThisFeature::Adapters::Memory do
     end
 
     context "looking up a flag that is set to off" do
-      before { described_class.off!(flag_name) }
+      before { adapter.off!(flag_name) }
 
       it { is_expected.to be(true) }
     end
 
     context "looking up a flag that is set to on" do
-      before { described_class.on!(flag_name) }
+      before { adapter.on!(flag_name) }
 
       it { is_expected.to be(false) }
     end
 
     context "when given a flag name and a user" do
       it "qualifies the search" do
-        described_class.on!(flag_name, context: pseudo_user)
-        expect(described_class.off?(flag_name)).to be true
-        expect(described_class.off?(flag_name, context: pseudo_user2)).to be true
-        expect(described_class.off?(flag_name, context: pseudo_user)).to be false
+        adapter.on!(flag_name, context: pseudo_user)
+        expect(ThisFeature.flag(flag_name).off?).to be true
+        expect(ThisFeature.flag(flag_name, context: pseudo_user2).off?).to be true
+        expect(ThisFeature.flag(flag_name, context: pseudo_user).off?).to be false
       end
     end
   end
 
-  describe '.present?' do
-    subject(:present?) { described_class.present?(flag_name) }
+  describe '#present?' do
+    subject(:present?) { adapter.present?(flag_name) }
 
     it { is_expected.to be(false) }
 
     context 'when flag is enabled' do
-      before { described_class.on!(flag_name) }
+      before { adapter.on!(flag_name) }
 
       it { is_expected.to be(true) }
     end
 
     context 'when flag is disabled' do
-      before { described_class.off!(flag_name) }
+      before { adapter.off!(flag_name) }
 
       it { is_expected.to be(true) }
     end
   end
 
   describe '.on!' do
-    subject(:on!) { described_class.on!(flag_name) }
+    subject(:on!) { adapter.on!(flag_name) }
 
     it 'turns on the flag' do
       subject
-      expect(described_class.on?(flag_name)).to eq(true)
+      expect(adapter.on?(flag_name)).to eq(true)
     end
 
     context 'with a user passed as the context' do
-      subject(:on!) { described_class.on!(flag_name, context: pseudo_user) }
+      subject(:on!) { adapter.on!(flag_name, context: pseudo_user) }
 
       it 'turns on the flag for the specific user' do
         subject
 
-        expect(described_class.present?(flag_name)).to be(true)
-        expect(described_class.on?(flag_name)).to be(false)
-        expect(described_class.on?(flag_name, context: pseudo_user)).to be(true)
-        expect(described_class.on?(flag_name, context: pseudo_user2)).to be(false)
+        expect(adapter.present?(flag_name)).to be(true)
+        expect(adapter.on?(flag_name)).to be(false)
+        expect(adapter.on?(flag_name, context: pseudo_user)).to be(true)
+        expect(adapter.on?(flag_name, context: pseudo_user2)).to be(false)
       end
     end
   end
 
   describe '.off!' do
-    subject(:off!) { described_class.off!(flag_name) }
+    subject(:off!) { adapter.off!(flag_name) }
 
     it 'turns off the flag' do
       subject
-      expect(described_class.off?(flag_name)).to eq(true)
+      expect(adapter.off?(flag_name)).to eq(true)
     end
 
     context 'with a user passed as the context' do
-      subject(:off!) { described_class.off!(flag_name, context: pseudo_user) }
+      subject(:off!) { adapter.off!(flag_name, context: pseudo_user) }
 
       it 'turns off the flag for the specific user' do
         subject
 
-        expect(described_class.present?(flag_name)).to be(true)
-        expect(described_class.off?(flag_name)).to be(true)
-        expect(described_class.off?(flag_name, context: pseudo_user)).to be(true)
-        expect(described_class.off?(flag_name, context: pseudo_user2)).to be(true)
+        expect(adapter.present?(flag_name)).to be(true)
+        expect(adapter.off?(flag_name)).to be(true)
+        expect(adapter.off?(flag_name, context: pseudo_user)).to be(true)
+        expect(adapter.off?(flag_name, context: pseudo_user2)).to be(true)
       end
     end
   end
