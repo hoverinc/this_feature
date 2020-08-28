dd88f7680f07511f525c769cdf240de8340b24cc
matthew fong <matthew@hover.to>
2020-08-28 16:38:14 -0700

    split io adapter specs

--- lib/this_feature/adapters/split_io.rb
+++ lib/this_feature/adapters/split_io.rb
@@ -3,45 +3,45 @@ require 'splitclient-rb'
 class ThisFeature
   module Adapters
     class SplitIo < Base
-      class << self
+      UNDEFINED_KEY = 'undefined_key'
 
-        def setup(split_client:)
-          @@split_client = split_client
-          split_client.block_until_ready
-        rescue SplitIoClient::SDKBlockerTimeoutExpiredException
-          raise 'Split SDK is not ready. Abort execution.'
+      def initialize(client:)
+        @client = client
+        client.block_until_ready
+      rescue SplitIoClient::SDKBlockerTimeoutExpiredException
+        raise 'Split SDK is not ready. Abort execution.'
+      end
+
+      def present?(flag_name)
+        !control?(flag_name)
+      end
+
+      def control?(flag_name, context: UNDEFINED_KEY, data: {})
+        treatment(flag_name, context: context, data: data).eql?('control_treatment')
+      end
+
+      def on?(flag_name, context: UNDEFINED_KEY, data: {})
+        treatment(flag_name, context: context, data: data).eql?('on')
+      end
+
+      def off?(flag_name, context: UNDEFINED_KEY, data: {})
+        treatment(flag_name, context: context, data: data).eql?('off')
+      end
+
+      private
+
+      attr_reader :client
+
+      def treatment(flag_name, context: UNDEFINED_KEY, data: {})
+        key = if context.nil?
+          UNDEFINED_KEY
+        elsif context.respond_to?(:to_s)
+          context.to_s
+        else
+          context
         end
 
-        def present?(flag_name)
-          !control?(flag_name, context: 'undefined')
-        end
-
-        def control?(flag_name, context:, data: {})
-          treatment(flag_name, context:, data).eql?('control')
-        end
-
-        def on?(flag_name, context:, data: {})
-          treatment(flag_name, context:, data).eql?('on')
-        end
-
-        def off?(flag_name, context:, data: {})
-          treatment(flag_name, context:, data).eql?('off')
-        end
-
-        def on!(flag_name, context:, data: {})
-        end
-
-        def off!(flag_name, context:, data: {})
-        end
-
-        def treatment(flag_name, context:, data: {})
-          key = context.respond_to?(:to_s) ? context.to_s : context
-          client.get_treatment(key, flag_name, attributes)[:treatment]
-        end
-
-        def split_client
-          @@split_client
-        end
+        client.get_treatment(key, flag_name, data)
       end
     end
   end
