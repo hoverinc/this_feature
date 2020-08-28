70198de1319c9dc94e008ff1396f9e4bfaf116fb
matthew fong <matthew@hover.to>
2020-08-28 12:38:41 -0700

    fix indentation

--- lib/this_feature/adapters/split_io.rb
+++ lib/this_feature/adapters/split_io.rb
@@ -5,46 +5,43 @@ class ThisFeature
     class SplitIo < Base
       class << self
 
-      def setup(factory:)
-        @@factory = factory
-        client.block_until_ready
-      rescue SplitIoClient::SDKBlockerTimeoutExpiredException
-        raise 'Split SDK is not ready. Abort execution.'
-      end
+        def setup(split_client:)
+          @@split_client = split_client
+          split_client.block_until_ready
+        rescue SplitIoClient::SDKBlockerTimeoutExpiredException
+          raise 'Split SDK is not ready. Abort execution.'
+        end
 
-      def present?(flag_name)
-        !control?(flag_name, context: 'undefined')
-      end
+        def present?(flag_name)
+          !control?(flag_name, context: 'undefined')
+        end
 
-      def control?(...)
-        treatment(...).eql?('control')
-      end
+        def control?(flag_name, context:, data: {})
+          treatment(flag_name, context:, data).eql?('control')
+        end
 
-      def on?(...)
-        treatment(...).eql?('on')
-      end
+        def on?(flag_name, context:, data: {})
+          treatment(flag_name, context:, data).eql?('on')
+        end
 
-      def off?(...)
-        treatment(...).eql?('off')
-      end
+        def off?(flag_name, context:, data: {})
+          treatment(flag_name, context:, data).eql?('off')
+        end
 
-      def on!(...)
-      end
+        def on!(flag_name, context:, data: {})
+        end
 
-      def off!(...)
-      end
+        def off!(flag_name, context:, data: {})
+        end
 
-      def treatment(flag_name, context:, data: {})
-        key = context.respond_to?(:to_s) ? context.to_s : context
-        client.get_treatment(key, flag_name, attributes)[:treatment]
-      end
+        def treatment(flag_name, context:, data: {})
+          key = context.respond_to?(:to_s) ? context.to_s : context
+          client.get_treatment(key, flag_name, attributes)[:treatment]
+        end
 
-      def client
-        @@client ||= factory.client
-      end
-
-      def factory
-        @@factory
+        def split_client
+          @@split_client
+        end
       end
     end
   end
